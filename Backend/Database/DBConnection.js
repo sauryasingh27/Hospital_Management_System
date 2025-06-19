26617e628473de314bbdaab2d7d5d9dd226fb391
Saurya Singh <saurya397@gmail.com>
2025-06-19 17:55:57 +0530

    Fix: Updated DB connection string

--- Backend/Database/DBConnection.js
+++ Backend/Database/DBConnection.js
@@ -3,10 +3,10 @@ import mongoose from "mongoose";
 export const dbConnection = () => {
   mongoose
     .connect(process.env.MONGO_URI, {
-      dbName: "Hospital_Management_System_MERN_Project",
+      dbName: "hms_db",
     })
     .then(() => {
-      console.log("Connected to database!");
+      console.log("Connected to database!", mongoose.connection.name);
     })
     .catch((err) => {
       console.log("Error occured while connecting to database:", err);
